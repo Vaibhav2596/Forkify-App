088aaa035fadaacf0cc1c5eccd98a538b68853b0
Vaibhav2596 <chavanv214@gmail.com>
2023-09-23 00:28:13 +0530

    Initial Commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '9f19880f-0d20-4f34-a41a-09c75c57fe13';
+export const MODAL_CLOSE_SEC = 2.5;
